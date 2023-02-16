83b02ec5823c31a75269192dc7c292e697cf830e
Leonardo Uieda <leouieda@gmail.com>
2023-02-16 12:35:24 +0000

    Implement the inversion and uncertainty propagation
    Make some figures for the paper as well. Just missing the table output
    revision for the manuscript.

--- code/micromag.py
+++ code/micromag.py
@@ -17,7 +17,16 @@ TESLA_TO_NANOTESLA = 1e9
 MICROMETER_TO_METER = 1e-6
 
 
-def cartesian_vector(inclination, declination, amplitude):
+def gaussian_noise(error, shape, seed=None):
+    """
+    Generate a pseudo-random noise array of the given shape.
+    """
+    rng = np.random.default_rng(seed)
+    noise = rng.normal(loc=0, scale=error, size=shape)
+    return noise
+
+
+def angles_to_vector(inclination, declination, amplitude):
     """
     Generate a 3-component vector from inclination, declination, and amplitude
 
@@ -35,8 +44,10 @@ def cartesian_vector(inclination, declination, amplitude):
 
     Returns
     -------
-    x, y, z : floats or arrays
-        The calculated x, y, z vector components.
+    vector : 1D or 2D array
+        The calculated x, y, z vector components. 1D if it's a single vector.
+        If N vectors are calculated, the "vector" will have shape (N, 3) with
+        each vector in a row of the array.
     """
     inclination = np.radians(inclination)
     declination = np.radians(declination)
@@ -48,7 +59,40 @@ def cartesian_vector(inclination, declination, amplitude):
     x = cos_inc * sin_dec * amplitude
     y = cos_inc * cos_dec * amplitude
     z = sin_inc * amplitude
-    return x, y, z
+    return np.transpose([x, y, z])
+
+
+def vector_to_angles(vector):
+    """
+    Generate inclination, declination, and amplitude from a 3-component vector
+
+    Inclination is positive downwards and declination is the angle with the y
+    component. The vector has x, y, and z (upward) Cartesian components.
+
+    Parameters
+    ----------
+    vector : 1D or 2D array
+        The x, y, z vector components. Can be a 1D array for a single vector
+        or 2D for multiple. If 2D, then each vector should be a row of the
+        array.
+
+    Returns
+    -------
+    inclination : float or array
+        The inclination values in degrees.
+    declination : float or array
+        The declination values in degrees.
+    amplitude : float or array
+        The vector amplitude values.
+    """
+    vector = np.asarray(vector)
+    # if vector.ndim == 1:
+        # vector = np.array([vector])
+    x, y, z = vector.T
+    amplitude = np.sqrt(x**2 + y**2 + z**2)
+    inclination = -np.degrees(np.arctan2(z, np.hypot(x, y)))
+    declination = np.degrees(np.arctan2(x, y))
+    return inclination, declination, amplitude
 
 
 def dipole_bz(coordinates, dipole_coordinates, dipole_moments):
@@ -126,13 +170,118 @@ def _dipole_bu_fast(e, n, u, de, dn, du, dipole_moments, bu):
             bu[i] += result
 
 
-def gaussian_noise(error, shape, seed=None):
+def dipole_moment_inversion(data, dipole_coordinates):
     """
-    Generate a pseudo-random noise array of the given shape.
+    Estimate the dipole moment through linear inversion.
+
+    Parameters
+    ----------
+    data : xarray.DataArray
+        The observed vertical magnetic field grid. Must have x, y, and z as
+        coordinates.
+    dipole_coordinates : 1D array
+        The (x, y, z) coordinates of the dipole.
+
+    Returns
+    -------
+    dipole_moment : 1D array
+        The estimated dipole moment vector.
+    covariance : 2D array
+        The estimated covariance matrix of the dipole moment vector.
+    r2 : float
+        The R² coefficient of determination of the inversion.
     """
-    rng = np.random.default_rng(seed)
-    noise = rng.normal(loc=0, scale=error, size=shape)
-    return noise
+    # Make it a dataset so we can be sure the variable name is "bz"
+    table = vd.grid_to_table(data.to_dataset(name="bz"))
+    # Verde drops non-dimension coordinates so we have to add z back.
+    # This is a bug in Verde.
+    table["z"] = data.z.values.ravel()
+
+    n_data = table.shape[0]
+    n_params = 3
+    A = np.empty((n_data, n_params))
+    d = table.bz.values / TESLA_TO_NANOTESLA
+    # Fill the Jacobian using a fast calculation with numba
+    _dipole_jacobian_fast(
+        table.x.values * MICROMETER_TO_METER,
+        table.y.values * MICROMETER_TO_METER,
+        table.z.values * MICROMETER_TO_METER,
+        dipole_coordinates[0] * MICROMETER_TO_METER,
+        dipole_coordinates[1] * MICROMETER_TO_METER,
+        dipole_coordinates[2] * MICROMETER_TO_METER,
+        A,
+    )
+    hessian = A.T @ A
+    neg_gradient = A.T @ d
+    dipole_moment = np.linalg.solve(hessian, neg_gradient)
+
+    residuals = d - A @ dipole_moment
+    residuals_sum_sq = np.sum(residuals**2)
+    # Estimate of the true error variance (since we'll never know it)
+    chi_squared =  residuals_sum_sq / (n_data - n_params)
+    covariance = chi_squared * np.linalg.inv(hessian)
+    r2 = 1 - residuals_sum_sq / np.linalg.norm(d - d.mean())**2
+
+    return dipole_moment, covariance, r2
+
+
+@numba.jit(nopython=True, parallel=True)
+def _dipole_jacobian_fast(e, n, u, de, dn, du, jacobian):
+    """
+    This is the bit that runs the fast for-loops
+    """
+    constant = choclo.constants.VACUUM_MAGNETIC_PERMEABILITY / (4 * np.pi)
+    for i in numba.prange(e.size):
+        # Calculating the distance only once saves a lot of computation time
+        distance = choclo.utils.distance_cartesian(
+            e[i], n[i], u[i], de, dn, du,
+        )
+        # These are the second derivatives of 1/r
+        jacobian[i, 0] = constant * choclo.point.kernel_eu(
+            easting_p=e[i],
+            northing_p=n[i],
+            upward_p=u[i],
+            easting_q=de,
+            northing_q=dn,
+            upward_q=du,
+            distance=distance,
+        )
+        jacobian[i, 1] = constant * choclo.point.kernel_nu(
+            easting_p=e[i],
+            northing_p=n[i],
+            upward_p=u[i],
+            easting_q=de,
+            northing_q=dn,
+            upward_q=du,
+            distance=distance,
+        )
+        jacobian[i, 2] = constant * choclo.point.kernel_uu(
+            easting_p=e[i],
+            northing_p=n[i],
+            upward_p=u[i],
+            easting_q=de,
+            northing_q=dn,
+            upward_q=du,
+            distance=distance,
+        )
+
+
+def covariance_to_angle_std(dipole_moment, covariance):
+    """
+    Calculate the inc, dec, amp standard deviations from the covariance matrix
+    """
+    mx2, my2, mz2 = dipole_moment**2
+    m = np.linalg.norm(dipole_moment)
+    var_x, var_y, var_z = np.diag(covariance)
+    sigma_inc = np.degrees(
+        np.sqrt(
+            (mx2 * mz2 * var_x + my2 * mz2 * var_y + (mx2 + my2)**2 * var_z)
+            / ((mx2 + my2) * m**4)
+        )
+    )
+    sigma_dec = np.degrees(np.sqrt((my2 * var_x + mx2 * var_y) / (mx2 + my2)**2))
+    sigma_amp = np.sqrt((mx2 * var_x + my2 * var_y + mz2 * var_z) / m**2)
+    return sigma_inc, sigma_dec, sigma_amp
 
 
 def data_gradients(data):
